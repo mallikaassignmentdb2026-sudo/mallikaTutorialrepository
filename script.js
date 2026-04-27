71966e70fb63cbd647f63a4a46db691a61d56b29
mallikaassignmentdb2026-sudo <mallika.assignment.db2026@gmail.com>
2026-04-27 22:15:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+fetch("https://script.google.com/macros/s/AKfycbzSOzUeItC5nkSE42sSmBghulA6hqcwKrywsmIIX--66-z-c_tC_TMVtBeIpP1TcNjYfw/exec",
+      {method:"POST",
+       body: JSON.stringify(data)
+      })
